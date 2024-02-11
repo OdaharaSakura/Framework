175eabf36da708dff8c49c91a32ad90cde981020
OdaharaSakura <117719725+OdaharaSakura@users.noreply.github.com>
2024-02-11 22:25:48 +0900

    ローディング画面

--- loadinglogo.cpp
+++ loadinglogo.cpp
@@ -13,18 +13,15 @@ void LoadingLogo::Init()
 	Renderer::CreatePixelShader(&m_PixelShader,
 		"shader\\unlitTexturePS.cso");
 
-	m_Scale.x = SCREEN_WIDTH / 3 * 2;
-	m_Scale.y = SCREEN_HEIGHT / 3 * 2;
 
-	m_WorldPosition.x = (SCREEN_WIDTH / 2) - (m_Scale.x / 2);
-	m_WorldPosition.y = (SCREEN_HEIGHT / 3) - (m_Scale.y / 2) + 15.0f;
-
+	m_WorldPosition.x = 0.0f;
+	m_WorldPosition.y = 0.0f;
 
 	m_Sprite = AddComponent<Sprite>();
-	m_Sprite->Init(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT, "asset/texture/lightgreen.jpg");//è„ÇÃÇ∆Ç«Ç¡ÇøÇ≈Ç‡ÇÊÇ¢
+	m_Sprite->Init(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT, "asset/texture/Waitwindow.dds");//è„ÇÃÇ∆Ç«Ç¡ÇøÇ≈Ç‡ÇÊÇ¢
 
 	m_Sprite = AddComponent<Sprite>();
-	m_Sprite->Init(m_WorldPosition.x, m_WorldPosition.y, m_Scale.x, m_Scale.y, "asset/texture/Loading.png");//è„ÇÃÇ∆Ç«Ç¡ÇøÇ≈Ç‡ÇÊÇ¢
+	m_Sprite->Init(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT, "asset/texture/Loading.dds");//è„ÇÃÇ∆Ç«Ç¡ÇøÇ≈Ç‡ÇÊÇ¢
 
 
 }
